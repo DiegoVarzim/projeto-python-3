dd18f5b7b259dfaaf4a8c02bda917aeacd27c248
Diego Varzim <diego.varzim@hotmail.com>
2023-05-30 10:28:57 -0300

    first commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,16 @@
+import random # importando a biblioteca random do python
+
+user_points = 0
+computer_points = 0
+
+while True:
+    user_choice = input('Escolha R(Pedra)/T(Tesoura)/P(Papel) ou Q para sair.').lower()
+
+    if user_choice == 'q':
+        break
+
+    computer_choice = random.randint(0, 2)
+    # 0:R, 1:T, 2:P
+    
+print('Goodbye!')
+
